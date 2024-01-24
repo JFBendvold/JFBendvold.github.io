d58c3b04596189cfcbc8130a931fe7ad7ee0dc44
John Fredrik Bendvold <johnfb@stud.ntnu.no>
2024-01-24 12:52:45 +0100

    Fixed file-structure

---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+                    />
+                    <link rel="stylesheet" href="/globals.css" />
+                    <title>Lokal - lokale råvarer et tastetrykk unna</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
