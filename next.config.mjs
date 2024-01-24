8a51b6e0fb0ba3a5b99a8e8c2ee75973711db918
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2024-01-24 14:15:45 +0100

    Change configs related to deploy to ghp

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+
+    reactStrictMode: true,
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
 
 };
 
