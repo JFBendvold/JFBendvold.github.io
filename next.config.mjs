d45b69222227877312a0adf1f03503ddccbc4504
Magnus Lutro Allison <magnulal@stud.ntnu.no>
2024-01-25 08:35:30 +0100

    Add reactStrictMode and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     output: "export",
-    basePath: "/nextjs-github-pages",
     images: {
         unoptimized: true,
     }
